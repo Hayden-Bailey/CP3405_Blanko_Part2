272097c51451c7d86a7f6c6307f49d56988be224
Hayden-Bailey <hayden.bailey@my.jcu.edu.au>
2021-09-28 01:51:47 +1000

    Add initial code for QuestionActivity and CreateAccountActivity

---
+++ app/src/main/java/au/edu/jcu/cp3406/cp3405_blanko/CreateAccountActivity.java
@@ -0,0 +1,30 @@
+package au.edu.jcu.cp3406.cp3405_blanko;
+
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+
+public class CreateAccountActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_account);
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled(true);
+        }
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Respond to the action bar's Up/Home button
+        if (item.getItemId() == android.R.id.home) {
+            super.onBackPressed();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
--- app/src/main/java/au/edu/jcu/cp3406/cp3405_blanko/MainActivity.java
+++ app/src/main/java/au/edu/jcu/cp3406/cp3405_blanko/MainActivity.java
@@ -1,10 +1,10 @@
 package au.edu.jcu.cp3406.cp3405_blanko;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -21,6 +21,11 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled(true);
+        }
+
         firstName = findViewById(R.id.firstName);
         lastName = findViewById(R.id.lastName);
         Button signInButton = findViewById(R.id.signIn);
@@ -35,12 +40,20 @@ public class MainActivity extends AppCompatActivity {
                 String lastNameText = lastName.getText().toString();
                 boolean userExists = accountDatabaseHelper.checkUserAccounts(firstNameText,lastNameText);
                 if (userExists) {
-                    Intent intent = new Intent(MainActivity.this, QuestionFragment.class);
+                    Intent intent = new Intent(MainActivity.this, QuestionActivity.class);
+                    startActivity(intent);
                 }
                 else {
                     popupMessage();
-                    Log.i(TAG, "User doesn't exist. Please create an account");
                 }
+            }
+        });
+
+        createAccountButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, CreateAccountActivity.class);
+                startActivity(intent);
             }
         });
     }
