670ba16b260d2daa98561ef82a17d635d529dbc4
SeanNevilleJCU <sean.neville@my.jcu.edu.au>
2021-10-05 13:58:00 +1000

    Created Call Activity
    Links with dashboard call

--- app/src/main/java/au/edu/jcu/cp3406/cp3405_blanko/DashboardActivity.java
+++ app/src/main/java/au/edu/jcu/cp3406/cp3405_blanko/DashboardActivity.java
@@ -2,7 +2,9 @@ package au.edu.jcu.cp3406.cp3405_blanko;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class DashboardActivity extends AppCompatActivity {
 
@@ -10,5 +12,13 @@ public class DashboardActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
+
+        findViewById(R.id.Call).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(DashboardActivity.this, CallActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
