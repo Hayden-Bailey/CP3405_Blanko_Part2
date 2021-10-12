be00116abb1ca291022f436a7d4cafc1df6bb760
SeanNevilleJCU <sean.neville@my.jcu.edu.au>
2021-10-12 13:43:42 +1000

    created and linked dashboard activities

--- app/src/main/java/au/edu/jcu/cp3406/cp3405_blanko/DashboardActivity.java
+++ app/src/main/java/au/edu/jcu/cp3406/cp3405_blanko/DashboardActivity.java
@@ -6,7 +6,12 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
+import au.edu.jcu.cp3406.cp3405_blanko.apps.CalendarActivity;
+import au.edu.jcu.cp3406.cp3405_blanko.apps.ContactsActivity;
+import au.edu.jcu.cp3406.cp3405_blanko.apps.MessagesActivity;
+import au.edu.jcu.cp3406.cp3405_blanko.apps.EmailActivity;
 import au.edu.jcu.cp3406.cp3405_blanko.apps.CallActivity;
+import au.edu.jcu.cp3406.cp3405_blanko.apps.EventsActivity;
 
 public class DashboardActivity extends AppCompatActivity {
 
@@ -15,10 +20,50 @@ public class DashboardActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
 
+        findViewById(R.id.Calendar).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(DashboardActivity.this, CalendarActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        findViewById(R.id.Contacts).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(DashboardActivity.this, ContactsActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        findViewById(R.id.Messages).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(DashboardActivity.this, MessagesActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        findViewById(R.id.Email).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(DashboardActivity.this, EmailActivity.class);
+                startActivity(intent);
+            }
+        });
+
         findViewById(R.id.Call).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(DashboardActivity.this, CallActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        findViewById(R.id.Events).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(DashboardActivity.this, EventsActivity.class);
                 startActivity(intent);
             }
         });
--- app/src/main/java/au/edu/jcu/cp3406/cp3405_blanko/apps/MessagesActivity.java
+++ app/src/main/java/au/edu/jcu/cp3406/cp3405_blanko/apps/MessagesActivity.java
@@ -6,11 +6,11 @@ import android.os.Bundle;
 
 import au.edu.jcu.cp3406.cp3405_blanko.R;
 
-public class CalendarActivity extends AppCompatActivity {
+public class MessagesActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_calendar);
+        setContentView(R.layout.activity_messages);
     }
 }
